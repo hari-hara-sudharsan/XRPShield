444b771d3c043014da70f2cae2b5fdbab3543f3e
hari-hara-sudharsan <sudharsanj008gmail.com>
2026-08-13 17:06:29 +0530

    Fix media query closing braces and activate modern card design for Settings page

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -2,7 +2,7 @@ import { WalletManager } from './utils/wallet.js';
 import { updateActiveTreasury } from './utils/execution-modal.js';
 import { initGlobalSearch } from './components/search.js';
 import { renderHeaderNotifications } from './components/notifications.js';
-import { I18nEngine } from './utils/i18n.js?v=10';
+import { I18nEngine } from './utils/i18n.js?v=11';
 
 window.copyAddressWithToast = function(addr) {
     if (navigator.clipboard) {
